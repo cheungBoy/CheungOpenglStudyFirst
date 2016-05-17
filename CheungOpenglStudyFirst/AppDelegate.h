02464eb760498891b81c9f707854555ca1f0e40f
cheungBoy <cheung@golfpad.cn>
2016-05-17 17:14:19 +0800

    Initial Commit

---
+++ CheungOpenglStudyFirst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheungOpenglStudyFirst
+//
+//  Created by cheungBoy on 16/5/17.
+//  Copyright © 2016年 cheungBoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CheungOpenglStudyFirst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheungOpenglStudyFirst
+//
+//  Created by cheungBoy on 16/5/17.
+//  Copyright © 2016年 cheungBoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
